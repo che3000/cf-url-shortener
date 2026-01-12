18a8c1b5534695cce41e0cf1521b5422afbaaa2f
Ching-Yuan Huang <hcy6406@gmail.com>
2026-01-12 22:08:35 +0800

    fix: root page footer in center

--- src/templates/root.html.ts
+++ src/templates/root.html.ts
@@ -9,7 +9,7 @@ export const renderRootHTML = (author: string, contact: string) => `<!doctype ht
 		<p>這裡沒有內容。</p>
         <p>別在探索的時候遺失了自我。</p>
 	</div>
-	<footer style="position:fixed;bottom:1rem;width:100%;text-align:center;font-size:.75rem;color:#888">
+	<footer style="position:fixed;bottom:1rem;left:0;width:100%;text-align:center;font-size:.75rem;color:#888">
 			<p>${author ? author : ""}</p>
 			<p>${contact ? contact : ""}<p>
 	</footer>
